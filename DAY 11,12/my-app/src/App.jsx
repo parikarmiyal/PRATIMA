013c7062aa441c12e2cb02154a0ee9da3088f483
parikarmiyal <159272538+parikarmiyal@users.noreply.github.com>
2026-01-27 17:15:41 +0530

    google keep with add,delete,edit functions

--- DAY 11,12/my-app/src/App.jsx
+++ DAY 11,12/my-app/src/App.jsx
@@ -2,29 +2,40 @@ import React, { useState } from "react";
 function App() {
   const [todolist, settodolist] = useState([]);
   const [newtask, setnewtask] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+  const [editValue, setEditValue] = useState("");
   const handlechange = (event) => {
     setnewtask(event.target.value);
   }
   const addtask = () => {
     settodolist([...todolist, newtask]);
   }
+  const deletetask = (taskname) => {
+    settodolist(todolist.filter((task) => task !== taskname));
+  }
+  const editTask = (index) => {
+    setEditIndex(index);
+    setEditValue(todolist[index]);
+  }
+  const saveTask = (index) => {
+    const updatedList = [...todolist];
+    updatedList[index] = editValue;
+    settodolist(updatedList);
+    setEditIndex(null);
+  }
   return (
     <div className="h-screen w-screen bg-white text-black">
-
       {/* Top Bar */}
       <header className="h-16 flex items-center justify-between px-6 gap-18">
         <div className="flex items-center gap-4">
           <span className="text-2xl">☰</span>
           <span className="text-xl font-semibold">🟨 Keep</span>
         </div>
-
         <input
           type="text"
           placeholder="Search"
-          className="flex-1  px-4 py-2 rounded-lg bg-blue-200 text-black"
-
+          className="flex-1  px-4 py-2 rounded-lg bg-gray-200 text-black"
         />
-
         <div className="flex items-center gap-4">
           <span>⟳</span>
           <span>⚙️</span>
@@ -33,10 +44,8 @@ function App() {
           </div>
         </div>
       </header>
-
       {/* Main Layout */}
       <div className="flex justify-between">
-
         {/* Sidebar */}
         <aside className="w-60 pt-4">
           <div className="px-6 py-3 ">
@@ -55,45 +64,53 @@ function App() {
              🗑   Bin
           </div>
         </aside>
-
         {/* Content */}
-        <main className="flex-1 flex flex-col items-center mt-10">
-
+        <main className="flex-1 flex flex-col items-center">
           {/* Input box */}
           <div className="w-150  rounded-lg px-4 py-3 flex items-center gap-4">
             <input
               type="text"
               placeholder="Take a note..."
               onChange={handlechange}
-              className="flex-1  px-4 py-2 rounded-lg bg-blue-200 text-black"
+              className="flex-1  px-4 py-2 rounded-lg bg-gray-300 text-black"
             />
             <button
               onClick={addtask}
-              className="text-blue-200 font-semibold"
+              className="text-gray-200 font-semibold"
             >
               Add note
             </button>
           </div>
-
           {/* Notes list (BELOW input) */}
-          <div className="w-1/2 mt-10 px-10 grid grid-rows gap-4  text-black">
+          <div className="w-1/2 px-10 grid grid-rows gap-4  text-black">
             {todolist.map((item, index) => (
               <div
                 key={index}
-                className="p-4 rounded-lg shadow bg-white"
+                className=" flex flex-row justify-between items-center p-4 rounded-lg shadow bg-blue-100 "
               >
-                {item}
+                {editIndex === index ? (
+              <>
+                <input className="flex-1  px-4 py-2 rounded-lg bg-gray-300 text-black"
+                  value={editValue}
+                  onChange={(e) => setEditValue(e.target.value)}
+                />
+                <button onClick={() => saveTask(index)} className="text-gray-200 font-semibold">Save</button>
+              </>
+            ) :(
+              <div className="flex flex-row gap-60">
+                <span>{item}</span>
+                <div className="flex flex-row gap-10">
+                <button onClick={() => deletetask(item)} className="text-gray-200 font-semibold"> Delete </button>
+                <button onClick={() => editTask(index) }className="text-gray-200 font-semibold">Edit</button>
+                </div>
+                </div>
+            )}
               </div>
-
             ))}
           </div>
-
         </main>
-
-
       </div>
     </div>
   );
 }
-
 export default App;
